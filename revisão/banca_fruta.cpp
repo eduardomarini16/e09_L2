a7761c13c86e409fa57f02be699635d82a7db884
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2025-08-09 16:27:28 -0300

    EX banca_fruta

---
+++ revisão/banca_fruta.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    int laranja;
+    float preco;
+
+    cout << "Digite a quantidade de laranjas que deseja comprar: ";
+    cin >> laranja;
+
+    if (laranja < 12) {
+        preco = 0.80;
+    }
+    else {
+        preco = 0.65;
+    }
+
+    cout << fixed << setprecision(2);
+    cout << "Total de " << laranja << " laranja(s), cada unidade custara: R$" << preco << endl;
+    cout << "Valor total da compra sera de: R$" << (laranja * preco) << endl;
+}
+
