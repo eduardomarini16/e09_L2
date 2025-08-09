c692ba63ef0e0758beb22df2cc021ea5da396712
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2025-08-09 16:50:09 -0300

    EX resto divisao

---
+++ revisão/0_50.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Digite um valor entre 1 - 50" << endl;
+    cin >> n;
+
+    while(n < 0 || n > 50) {
+        cout << "Valor incorreto, digite um valor entre 1 - 50" << endl;
+        cin >> n;
+    }
+
+    for (int i = n-1; i > 0; i--) {
+        cout << "Resto da divisao de " << n << " por " << i << " : " << n%i << endl;
+    }
+ }
