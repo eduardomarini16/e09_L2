48551d371b41dbcccb10956381f67204a63d761b
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2025-08-14 16:26:11 -0300

    Removendo arquivos

--- exercicio_cap_3/lista_futebol.py
+++
@@ -1,8 +0,0 @@
-lista_camp = ["São Paulo", "São José", "Real Madrid", "Barcelona", "Internacional"]
-
-print(lista_camp[1:4])
-print(lista_camp[-2:])
-print(sorted(lista_camp))
-print(lista_camp[3])
-
-
